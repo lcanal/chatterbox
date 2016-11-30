b074e39223ac4e6fd9f129214c926adbd9d276bc
Luis Canales <lacanales@ucdavis.edu>
2016-11-30 14:12:51 -0800

    Chap1 : Adding flag parsing for port.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"sync"
 	"text/template"
+	"flag"
 )
 
 //templ represents a single template
@@ -20,10 +21,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application")
+	flag.Parse() // Parge them flags
+
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
@@ -33,7 +37,7 @@ func main() {
 	go r.run()
 
 	//Start the web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe Error: ", err)
 	}
 }
