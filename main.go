b62fcc80ea0044928bcbda6501f3f2c5fe182601
Luis Canales <lacanales@ucdavis.edu>
2016-11-30 13:13:33 -0800

    Chap1 : Fully realized ish web chat server.

--- main.go
+++ main.go
@@ -24,7 +24,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room",r)
+
+	//Get the room join
+
+	go r.run()
 
 	//Start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
