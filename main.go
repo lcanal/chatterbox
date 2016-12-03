ee24ef0a9c93c72d884273d5118ceb0b87b52d23
Luis Canales <cluchomucho@gmail.com>
2016-12-03 14:49:13 -0800

    Chap2 : Adding name and authentication support. Missing keys. dont add!

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"github.com/lcanal/chatterbox/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 	"log"
 	"net/http"
 	"os"
@@ -23,13 +26,25 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application")
 	flag.Parse() // Parge them flags
 
+	//set up gomniauth
+	gomniauth.SetSecurityKey("x")
+	gomniauth.WithProviders(
+		google.New("x", "x",
+			"http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
